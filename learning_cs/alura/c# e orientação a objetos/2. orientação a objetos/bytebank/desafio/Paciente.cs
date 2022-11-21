40a0ffd17300b2cb250c9c43648b54a3bd20e560
Ana Kniss <ackniss@gmail.com>
2022-11-21 18:38:31 -0300

    Added course challenge

---
+++ learning_cs/alura/c# e orientação a objetos/2. orientação a objetos/bytebank/desafio/Paciente.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Principal;
+
+namespace desafio;
+
+public class Paciente
+{
+    public string nome;
+    public int idade;
+    public double peso;
+}
---
+++ learning_cs/alura/c# e orientação a objetos/2. orientação a objetos/bytebank/desafio/Program.cs
@@ -0,0 +1,10 @@
+﻿using desafio;
+
+Paciente paciente1 = new Paciente();
+paciente1.nome = "Ana";
+paciente1.idade = 24;
+paciente1.peso = 55;
+
+Console.WriteLine("Nome da paciente: " + paciente1.nome);
+Console.WriteLine("Idade do paciente: " + paciente1.idade);
+Console.WriteLine("Peso da paciente: " + paciente1.peso);
