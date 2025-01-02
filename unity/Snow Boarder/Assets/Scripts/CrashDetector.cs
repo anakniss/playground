138af4bf0ab6ea0bdbd29d5c812e2c9e6357da5f
Ana Kniss <ackniss@gmail.com>
2025-01-02 00:36:06 -0300

    initialize project

---
+++ unity/Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ unity/Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
