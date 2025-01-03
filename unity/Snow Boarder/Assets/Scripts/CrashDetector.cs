34e1da4896723e44decc778ac721a2cd9c1a9b0d
Ana Kniss <ackniss@gmail.com>
2025-01-02 23:02:09 -0300

    add new assets and effects

--- unity/Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ unity/Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -3,11 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem crashEffect;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
-            SceneManager.LoadScene(0);
+            crashEffect.Play();
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- unity/Snow Boarder/Assets/Scripts/FinishLine.cs
+++ unity/Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -3,11 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem finishEffect;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if(other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(0);
+            finishEffect.Play();
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+    
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
